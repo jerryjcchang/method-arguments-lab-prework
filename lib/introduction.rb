fc11334fa4db90c1c50340f8edc966c4490a98f4
JC Chang <jerryjcchang@gmail.com>
2018-10-06 18:39:45 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction(name)
-  puts "Hello my name is #{name}"
+  puts "Hello my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Hello ny name is #{name} and I am "
+  puts "Hello ny name is #{name} and I am learning how to program in #{language}."
